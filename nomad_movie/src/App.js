f8ef22024d965ea4912364db6b99eb85e7ca66b3
gray0108 <jiung0108@gmail.com>
2022-01-22 18:32:13 +0900

    practice how to using useEffect.(2)

--- nomad_movie/src/App.js
+++ nomad_movie/src/App.js
@@ -2,14 +2,33 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("")
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("I run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("I run only once.");
-  };
-  useEffect(iRunOnlyOnce, []);// 매번 render 시키고 싶지 않을때 사용. 한번만 call
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5){
+      console.log("I run when 'keyword' changed.");   
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changed.");   
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'counter and keyword' changed.");   
+  }, [counter,keyword]);
+
   return (
     <div>
+      <input 
+        onChange={onChange}
+        value={keyword}
+        type="text" 
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
