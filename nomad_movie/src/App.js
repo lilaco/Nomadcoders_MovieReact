29fe3423a8b6ac22b3e4b5dcae9be01db34d1ca0
gray0108 <jiung0108@gmail.com>
2022-01-22 12:07:39 +0900

    practice how to use useeffect.

--- nomad_movie/src/App.js
+++ nomad_movie/src/App.js
@@ -1,11 +1,17 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("I run only once.");
+  };
+  useEffect(iRunOnlyOnce, []);// 매번 render 시키고 싶지 않을때 사용. 한번만 call
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
