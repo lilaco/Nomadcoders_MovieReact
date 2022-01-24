31ee6f5e6f6d6a2464613c679d9367f506c9a298
gray0108 <jiung0108@gmail.com>
2022-01-24 21:16:14 +0900

    Making To Do List part 2

--- nomad_movie/src/App.js
+++ nomad_movie/src/App.js
@@ -25,6 +25,13 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+           <li key={index}>{item}</li>
+        ))}
+      </ul>
+  
     </div>
   );
 }
